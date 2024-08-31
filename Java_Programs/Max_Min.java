7e52f775fe24007b9278acd6bcdc998e06f196ff
Swaraj-001 <162905829+Swaraj-001@users.noreply.github.com>
2024-08-31 23:40:46 +0530

    Update Max_Min.java

--- Java_Programs/Max_Min.java
+++ Java_Programs/Max_Min.java
@@ -4,28 +4,22 @@ public class Max_Min {
     public static void main(String[] arg) {
         int N1 = 0, N2 = 0;
         Scanner s = new Scanner(System.in);
-        System.out.println(" ");
-        System.out.print("Enter 1st Number : ");
+        System.out.print("\n\t\t Enter 1st Number :\t");
         N1 = s.nextInt();
-        System.out.println(" ");
-        System.out.print("Enter 2nd Number : ");
+        System.out.print("\n\t\t Enter 2nd Number :\t");
         N2 = s.nextInt();
 
         if (N1 == N2) {
-            System.out.println(" ");
-            System.out.println("Both numbers are equal");
-            System.out.println(" ");
+            System.out.println("\n\t\t Both numbers are equal. \n");
         } else if (N1 > N2) {
-            System.out.println(" ");
-            System.out.println("Number " + N1 + " is Maximum");
-            System.out.println(" ");
+            System.out.println("\n\t\t Number " + N1 + " is Maximum. \n");
         } else {
-            System.out.println(" ");
-            System.out.println("Number " + N2 + " is Maximum");
-            System.out.println(" ");
+            System.out.println("\n\t\t Number " + N2 + " is Maximum. \n");
         }
-        System.out.println("!!!...Thank You...!!!");
+        System.out.println("\n\t\t!!!...Thank_You...!!! \n");
         System.out.println(" ");
+
+        s.close();
     }
 
 }
