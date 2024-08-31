d8559e792795b64d416ef6b69837488167372f87
Swaraj-001 <162905829+Swaraj-001@users.noreply.github.com>
2024-09-01 00:59:17 +0530

    Update Add.java

--- Java_Programs/Add.java
+++ Java_Programs/Add.java
@@ -4,18 +4,14 @@ public class Add {
     public static void main(String[] args) {
         int N1 = 0 , N2 = 0 ,sum = 0;
         Scanner s = new Scanner(System.in);
-        System.out.println(" ");
-        System.out.print("Enter 1st Number : ");
+        System.out.print("\n\t\t Enter 1st Number : \t");
         N1 = s.nextInt();
-        System.out.println(" ");
-        System.out.print("Enter 2nd Number : ");
+        System.out.print("\n\t\t Enter 2nd Number : \t");
         N2 = s.nextInt();
-        System.out.println(" ");
         sum = N1+N2;
-        System.out.println("Addition of "+N1+" and "+N2+" is "+sum);
-        System.out.println(" ");
-        System.out.println(" !!!... Thank You ...!!!");
-        System.out.println(" ");
-
+        System.out.println("\n\n\t\t Addition of "+N1+" and "+N2+" is "+sum );
+        System.out.println("\n\n\t\t!!!... Thank You ...!!!\n");
+        s.close();
     }    
+
 }
