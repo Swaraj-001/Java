d357915d0f08570182881308423b268c6f44cfdc
Swaraj-001 <162905829+Swaraj-001@users.noreply.github.com>
2024-09-01 00:53:03 +0530

    Update Day_Name.java

--- Java_Programs/Day_Name.java
+++ Java_Programs/Day_Name.java
@@ -1,38 +1,46 @@
-import java.time.*;
 import java.util.*;
 
-public class Day_Name {
-    public static void main(String[] args) {
+public class Day_Name 
+{
+    public static void main() 
+    {
         int Day = 0;
-        System.out.print("\n Enter Day Number to Explore:  ");
+        String dayString;
+        
+        System.out.print("\n\t\t Enter Day Number to Explore:  ");
+       
         Scanner s = new Scanner(System.in);
         Day = s.nextInt();
-        switch (Day) {
-            case 1:
-                System.out.println("\n The Day is Monday");
-                break;
-            case 2:
-                System.out.println("\n The Day is Tuesday");
-                break;
-            case 3:
-                System.out.println("\n The Day is Wednesday");
-                break;
-            case 4:
-                System.out.println("\n The Day is Thursday");
-                break;
-            case 5:
-                System.out.println("\n The Day is Friday");
-                break;
-            case 6:
-                System.out.println("\n The Day is Saturday");
-                break;
-            case 7:
-                System.out.println("\n The Day is Sunday");
-                break;
 
+        switch (Day) 
+        {
+            case 1:
+                    dayString = "Monday";
+                    break;
+            case 2:
+                    dayString = "Tuesday";
+                    break;
+            case 3:
+                    dayString = "Wednesday";
+                    break;
+            case 4:
+                    dayString = "Thursday";
+                    break;
+            case 5:
+                    dayString = "Friday";
+                    break;
+            case 6:
+                    dayString = "Saturday";
+                    break;
+            case 7:
+                    dayString = "Sunday";
+                    break;
             default:
-                System.out.println("\n Day Number Invalid");
+                    dayString = "Day Number Invalid";
         }
+        System.out.println("\n\t\t\t The Day is "+dayString+". ");
+        System.out.println("\n\t\t\t !!!...Thank_You...!!!\n");
+        s.close();
     }
 
 }
