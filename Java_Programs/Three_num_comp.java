cf80a7b734aaeb9fee91c5b1c951279b1f6fca72
Swaraj-001 <162905829+Swaraj-001@users.noreply.github.com>
2024-08-31 22:08:48 +0530

    Update Three_num_comp.java

--- Java_Programs/Three_num_comp.java
+++ Java_Programs/Three_num_comp.java
@@ -4,29 +4,26 @@ public class Three_num_comp {
     public static void main(String arg[]) {
         int a = 0, b = 0, c = 0;
         Scanner s = new Scanner(System.in);
-        System.out.println(" ");
-        System.out.print("Enter the first number :");
+        System.out.print("\n\t Enter the first number :\t");
         a = s.nextInt();
-        System.out.println(" ");
-        System.out.print("Enter the second number :");
+        System.out.print("\n\t Enter the second number :\t");
         b = s.nextInt();
-        System.out.println(" ");
-        System.out.print("Enter the third number :");
+        System.out.print("\n\t Enter the third number :\t");
         c = s.nextInt();
 
         if (c == b && b == a) {
-            System.out.println("  ");
-            System.out.println("    All Numbers are Same");
+            System.out.println("\n\t\t\t All Numbers are Same");
         } else if (a >= b && a >= c) {
-            System.out.println(" ");
-            System.out.println("    " + a + " is greater");
+            System.out.println(" \n\t\t\t " + a + " is greater");
         } else if (b >= a && b >= c) {
-            System.out.println(" ");
-            System.out.println("    " + b + " is greater");
+            System.out.println(" \n\t\t\t " + b + " is greater");
         } else if (c >= b && c >= a) {
-            System.out.println(" ");
-            System.out.println("    " + c + " is greater");
+            System.out.println(" \n\t\t\t " + c + " is greater");
         }
+
+        System.out.println(" \n\t\t !!!...Thank_You...!!! \n");
+        s.close();
+    
     }
 
 }
