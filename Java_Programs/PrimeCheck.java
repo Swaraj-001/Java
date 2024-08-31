6c14a9377ea1d22e7f5556ad25d06186c341902b
Swaraj-001 <162905829+Swaraj-001@users.noreply.github.com>
2024-08-31 22:41:05 +0530

    Update PrimeCheck.java

--- Java_Programs/PrimeCheck.java
+++ Java_Programs/PrimeCheck.java
@@ -4,7 +4,7 @@ public class PrimeCheck {
     public static void main(String[] args) {
         int n = 0, a = 0, i = 2;
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.print("\n\t\t Enter a number :\t");
         n = s.nextInt();
         while (i <= n / 2) {
             if (n % i == 0) {
@@ -14,11 +14,13 @@ public class PrimeCheck {
                 i++;
         }
         if (a == 1) {
-            System.out.println(n + " is not a prime number");
+            System.out.println("\n\t\t Number " + n + " is not a prime number");
         }
         else {
-            System.out.println(n + " is a prime number");
+            System.out.println("\n\t\t Number " + n + " is a prime number");
         }
+        System.out.println("\n\t\t\t !!!...Thank_You...!!!");
+        s.close();
     }
                         
 }
