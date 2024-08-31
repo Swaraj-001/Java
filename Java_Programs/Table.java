5370372ecc10a617d3895c286af35bc6248eb104
Swaraj-001 <162905829+Swaraj-001@users.noreply.github.com>
2024-08-31 22:31:38 +0530

    Update Table.java

--- Java_Programs/Table.java
+++ Java_Programs/Table.java
@@ -4,16 +4,16 @@ public class Table {
     public static void main(String[] args) {
         int N1 = 0, i = 1, a = 0;
         Scanner s = new Scanner(System.in);
-        System.out.println(" ");
-        System.out.print("Enter a number: ");
+        System.out.print("\n\n\t\t Enter a number :\t");
         N1 = s.nextInt();
-        System.out.println(" ");
+        System.out.print(" ");
 
         while (i <= 10) {
             a = N1 * i;
-            System.out.println(N1 + " x " + i + " = " + a);
-            System.out.println(" ");
+            System.out.println("\n\t\t\t " + N1 + " x " + i + " = " + a);
             i++;
         }
+        System.out.println("\n\t\t !!!...Thank_You...!!!\n");
+        s.close();
     }
 }
